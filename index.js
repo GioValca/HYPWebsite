d8ed69d7fc981d2106504acd1986f8dd4b032469
edoardorealini <44018314+edoardorealini@users.noreply.github.com>
2020-05-23 14:34:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,10 +49,3 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
   });
 
 });
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
