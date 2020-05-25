387c9db4172a03b80943dcfe0cb72a74bd08090e
Madsomma <Madsomma@users.noreply.github.com>
2020-05-25 19:46:43 +0200

    all people

--- public/assets/js/allPeople.js
+++ public/assets/js/allPeople.js
@@ -1,113 +1,74 @@
-//getting the month to display from the cache
 console.log("Showing all People");
 
 $(document).ready(function(){
     
     var title = document.getElementById("page-title");
 
-    title.innerHTML = "All of our People!";
-
-    fetch("https://hyp-ave.herokuapp.com/v2/people").then(function(response){
-        return response.json();
-    }).then(function(json){
-        console.log(json);
-
-        var list_len = json.length;
-
-        if(list_len == 0){
-            title.innerHTML = "There are no People stored";
-            var col_container = document.getElementById("col-container");
-            var back_button = $("<button />")
-                                .addClass("btn btn-info")
-                                .attr("onclick", "window.location =" + " \"" +  "./index.html"  +" \"")
-                                .text("Go back to the home page");
-
-            back_button.appendTo(col_container);
-            
-            return;
-        }
-
-        var col_in_use = 1;
-
-        var col1 = document.getElementById("col1");
-        var col2 = document.getElementById("col2");
-
-        //put a cards respectively into one column and another!
-
-        for(i = 0; i < list_len; i++){
-            let {personId, nameAndSurname, birthday, picturePath, telephone, email, descriptionText, profession, role} = json[i];
-            if(col_in_use == 1){
-                var card = createPersonCard(personId, nameAndSurname, role, picturePath);
-                card.appendTo(col1);
-                console.log("appending card to col 1");
-                col_in_use = 2;
-                continue
-            }
-
-            if(col_in_use == 2){
-                var card = createPersonCard(personId, nameAndSurname, role, picturePath);
-                card.appendTo(col2);
-                console.log("appending card to col 2");
-                col_in_use = 1;
-                continue
-            }
-
-        }
-
+    title.innerHTML = "Meet the members of AVE";
+	
+	$.ajax({
+        type: 'GET',
+        url: 'https://hyp-ave.herokuapp.com/v2/people/',
+        dataType: 'json',
+        success: function(json) {
+					
+			$.each(json, function(index, person){
+				
+				role = person.role;
+				
+				console.log(role);
+				
+				switch(role) {
+					case "President":
+						var card = createPersonCard(person.personId, person.nameAndSurname, person.picturePath, "350px");
+						$("#president").append(card);
+						break;
+					case "Vice-President":
+						var card = createPersonCard(person.personId, person.nameAndSurname, person.picturePath, "300px");
+						$("#vice-pres").append(card);
+						break;
+					case "Secretary":
+						var card = createPersonCard(person.personId, person.nameAndSurname, person.picturePath, "300px");
+						$("#secretary").append(card);
+						break;
+					case "Councilor":
+						var card = createPersonCard(person.personId, person.nameAndSurname, person.picturePath, "250px");
+						var col = $('<div />').addClass("col-md-auto");
+						card.appendTo(col);
+						$("#third-row").append(col);
+						break;		
+				}
+				
+			});
+		}
     });
-
 });
 
-function goToPerson(personId){
-    console.log("Going to person ".concat(personId));
-    personId = String(personId);
-    //window.sessionStorage.setItem('person_to_display', personId);
-    window.location= "./person.html" + "?id=" + personId;
-}
 
-function createPersonCard(personId, personNameSurname, personRole, img_path){
-    var card = $('<div />')
-        .addClass("card mb-3")
-        .attr("id", personId);
+function createPersonCard(personId, name, img_path, max_width){
+    var container = $('<div />')
+        .addClass("card-box-a container_img top-10")
+        .attr("id", "person" + personId)
+		.attr("style", "max-width:" + max_width + ";");
+	
+	var link_img = $('<a />')
+		.attr("href", "./person.html" + "?id=" + personId)
+		.appendTo(container);
+	
+		$('<img />')
+			.attr("src", img_path)
+			.addClass("img-fluid")
+			.appendTo(link_img);
+	
+	var bottom = $('<div />')
+			.addClass("bottom_center")
+			.appendTo(container);
+	
+		$('<a />')
+			.attr("href", "./person.html" + "?id=" + personId)
+			.addClass("text-light")
+			.text(name.toLocaleUpperCase())
+			.appendTo(bottom);
 
-    var row = $('<div />')
-        .addClass("row no-gutters")
-        .appendTo(card);
-
-    var col4 = $('<div />')
-        .addClass("col-md-4")
-        .appendTo(row);
-    
-    $('<img />')
-        .attr('src', img_path)    //image relative path
-        .addClass("img-fluid card-img")
-        .width("100%").height("100%")
-        .appendTo(col4);
-
-    var col8 = $('<div />')
-        .addClass("col-md-8")
-        .appendTo(row)
-
-    var cardbody = $("<div />")
-        .addClass("card-body")
-        .appendTo(col8);
-    
-    $("<h5 />")
-        .addClass("card-title left-15")
-        .text(personNameSurname)
-        .appendTo(cardbody);
-        
-
-    $("<p />")
-        .addClass("card-text left-15")
-        .text(personRole)
-        .appendTo(cardbody);
-    
-        $("<button />")
-        .addClass("button-card btn btn-info left-15")
-        .appendTo(cardbody)
-        .attr("onclick", "goToPerson(" + personId + ")")
-        .text("More about ".concat(personNameSurname));
-
-    return card;
+    return container;
 }
